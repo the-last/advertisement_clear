7e9f3618e5e562513a2fbd8c3063b3606e8082ee
the-last <1039107092@qq.com>
2018-10-21 22:22:58 +0800

    improve

--- refuse_iframe.js
+++ refuse_iframe.js
@@ -1,2 +1,10 @@
-setInterval(() => {[].__proto__.slice.call(document.querySelectorAll('iframe')).forEach(i => i.setAttribute('style', 'display: none'))}, 2000)
+setInterval(() => {
+    document.getElementsByClassName('pulllog-box')[0].setAttribute('style', 'display:none');
+    var arr_clear = [].__proto__.slice.call(document.querySelectorAll('iframe'))
+    var arr_clear_2 = [].__proto__.slice.call(document.getElementsByTagName('info-div'))
+
+    var arr = [...arr_clear, ...arr_clear_2]
+    arr.forEach(i => i.setAttribute('style', 'display: none'))
+
+    }, 2000)
 console.log('iframe clear')
