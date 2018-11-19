05b6b21c0e12731635e898f9573904aa6905a75e
the-last <1039107092@qq.com>
2018-11-19 22:37:59 +0800

    add jianshu

--- refuse_iframe.js
+++ refuse_iframe.js
@@ -1,11 +1,17 @@
 setInterval(() => {
-    document.getElementsByClassName('pulllog-box')[0].setAttribute('style', 'display:none');
-    var arr_clear = [].__proto__.slice.call(document.querySelectorAll('iframe'))
-    var arr_clear_2 = [].__proto__.slice.call(document.getElementsByTagName('info-div'))
-    var arr_clear_3 = [].__proto__.slice.call(document.getElementsByClassName('fourth_column'))
-
-    var arr = [...arr_clear, ...arr_clear_2, ...arr_clear_3]
+    var arr_clear = [], arr_clear_2 = []
+    if (window.location.hostname === 'blog.csdn.net') {
+        document.getElementsByClassName('pulllog-box')[0].setAttribute('style', 'display:none');
+        arr_clear = arr_clear.concat([].__proto__.slice.call(document.querySelectorAll('iframe')))
+        arr_clear = arr_clear.concat([].__proto__.slice.call(document.getElementsByTagName('info-div')))
+        arr_clear = arr_clear.concat([].__proto__.slice.call(document.getElementsByClassName('fourth_column')))
+    }
+    if (window.location.hostname === 'www.jianshu.com') {
+        arr_clear_2.push(document.getElementById('note-fixed-ad-container'))
+    }
+    var arr = [...arr_clear, ...arr_clear_2]
+    console.log()
     arr.forEach(i => i.setAttribute('style', 'display: none'))
 
-    }, 2000)
+}, 2000)
 console.log('iframe clear')
